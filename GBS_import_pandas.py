e9147402c88d2036dd039245a0ab0044d6fc8531
schimar <martin.schilling@usu.edu>
2012-11-20 15:43:49 -0700

    simple filter loop working, now it has to be extended to the whole dataFrame...

--- GBS_import_pandas.py
+++ GBS_import_pandas.py
@@ -2,13 +2,13 @@
 import numpy as np
 import pandas as pd
 import scipy as sp
-data = pandas.read_csv("play.csv",index_col=0)
+#data = pandas.read_csv("play.csv",index_col=0)
 # file path if needed: /Users/paulwolf/Documents/Manuscripts_and_Projects/Aspen_GBS/UNEAK_8_Aug _12/aspen_hapmap/
-output_file = open("locus_dist_plot", 'w')
-df=pandas.DataFrame(data)
-not_N = df.values != 'N'
-col_sums = scipy.sum(not_N, axis=0)
-row_sums = scipy.sum(not_N, axis=1)
+#output_file = open("locus_dist_plot", 'w')
+#df=pandas.DataFrame(data)
+#not_N = df.values != 'N'
+#col_sums = scipy.sum(not_N, axis=0)
+#row_sums = scipy.sum(not_N, axis=1)
 
 
 
@@ -17,16 +17,17 @@ row_sums = scipy.sum(not_N, axis=1)
 #    for ind in locus:
 #        if ind != 'N':
 #            inds_per_locus += 1
-    new_row = str(row) + "\n"
-    output_file.write(new_row)
-output_file.close()
+#    new_row = str(row) + "\n"
+
+ #   output_file.write(new_row)
+#output_file.close()
 
 #You can see a particular column by indexing its name:
 #print df.p8
 #print""
 #You can see rows by indexing the range. Note that you need to use numpy to view the array. Just the index works but it does not show under a print statement.
 #print "Four rows of DataFrame:", df['TP2':'TP5']
-row_set=np.array(df['TP2':'TP5'])
+#row_set=np.array(df['TP2':'TP5'])
 #print""
 #print "Now converted to np.array:"
 #print row_set
@@ -55,7 +56,7 @@ hmp.insert(1, 'allele_2', second_allele)
 
 #######
 
-!head -n 10 hmc_play
+#!head -n 10 hmc_play
 
 ### hmp ###
 for i in range(2,53):
@@ -72,7 +73,7 @@ print not_N[i]
 
 
 
-not_N = df.values != 'N'
+#not_N = df.values != 'N'
 
 
 for val in hmp.ix[:, 10]:
@@ -96,29 +97,33 @@ for val in hmc.ix[:, 1]:
 ###################### test w/ single columns
 a = hmc.ix[:,1].copy()
 b = hmp.ix[:,3].copy()[:30]
+a[0] = '5|0'
+a[1] = '0|7'
+b[0] = 'T'
+b[1] = 'C'
 
+############# simplified version (w/ just one series/list) working, now extend the whole thing to the whole pie
 single = []
-for base in b:
-    counts = (val.split('|') for val in a)
-    for j, [count_1, count_2] in enumerate(counts):
-        if base == 'N':
-            value = 'N'  # or write 'N' to the new df (hmp)!!
-        if base != 'N':
-            if int(count_1) < 4: # drop the base in new df
-                value = 'N'
-            if int(count_2) < 4:
-                value = 'N'
-            if int(count_1) >= 4: # write the according base into the new df
-                value = base
-            if int(count_2) >= 4:
-                value = base
+for i, base in enumerate(b):
+    count_1, count_2 = a[i].split('|')
+    if base == 'N':
+        value = 'N'
+    else:
+        if int(count_1) < 4 and int(count_2) < 4:
+            value = 'N'
+        elif int(count_1) < 4 and int(count_2) >= 4:
+            value = base # note: check allele_1 and allele_2 and ambiguity codes!
+        elif int(count_1) >= 4 and int(count_2) < 4:
+            value = base
+        else:
+            value = base # maybe here: check for ambiguity and potential alleles
     single.append(value)
-
-# --->>> fix the logic:
-#                       if value gets to be N when count_1 is below 0, then it is being overwritten with the secong allele...
+######################
 
 
-# --->>> if both count_1 and count_2 are >= 4    (!!!)
+
+
+
 # ambiguity codes
 
 # unused columns have to be cut out in order to just process the real stuff
@@ -140,5 +145,5 @@ df = pd.DataFrame({}, index = hmp.index[:30], columns=hmp.columns, dtype = np.st
 
 # 5) insert N's and bases into new df (in the loop or outside?
 
-## --->> probably at the start, specify the potential
+## --->> probably at the start, specify the potential bases allele_1 & allele_2
 
