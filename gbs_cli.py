3b0ccfc0cccb62619eb4646c6b23710c93f41adf
Your Name Here <martin.schilling@usu.edu>
2013-02-28 10:41:54 -0700

    bug in MAF filter

--- gbs_cli.py
+++ gbs_cli.py
@@ -108,10 +108,10 @@ def get_alleles_MAF(base_list, count_list, allele_list, MAF = 0.45):
 			value = str(allele_1 + '/' + allele_2)
 		    elif ((count_2 - count_1)/count_2) < MAF:
 			value = str(allele_2 + '/' + allele_2)
-		elif count_1 > 0 and count_2 == 0:
-		    value = str(allele_1 + '/' + allele_1) 
-		elif count_2 > 0 and count_1 == 0:
-		    value = str(allele_2 + '/' + allele_2)
+	    elif count_1 > 0 and count_2 == 0:
+		value = str(allele_1 + '/' + allele_1) 
+	    elif count_2 > 0 and count_1 == 0:
+		value = str(allele_2 + '/' + allele_2)
 	result.append(value)
     return result 
 
