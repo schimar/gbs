ac20be854c2ba875629fdaaff1b84a2cc6cac2a6
Your Name Here <martin.schilling@usu.edu>
2013-03-19 15:11:07 -0600

    hmp_replicate_ratios finished

--- Aarons_filter.py
+++ Aarons_filter.py
@@ -148,24 +148,130 @@ for pop in hmp_trim.columns:
 	pops.append(re.findall('([A-Z]+[0-9]+[a-z]+)', pop))
 
 rep_samples = np.unique(np.array(pops))
-###
 
-hmp.columns[rep_samples]
+############################################
+### NOTE: there are 9 3rd replicates !!!
+############################################
 
-for sample in rep_samples:
-    print sample
+### course of action: 
+
+# take the post-UNEAK replicates and filter those
 
 
+ # hmp 
+# 1) loop over columns and find the pairs of replicates
+# 2) compare them and count the number of True and False
+# 3) create a new dataFrame, with True and False counts
 
-for i, alleles in hmp_trim.FLFL08:
-    replica = hmp_trim.FLFL08replicate[i]
-    if alleles == replica:
-	print 'yes'
+
+# hmc
+
+# 1) loop over columns and find the pairs of replicates
+# 2) take count_1 and count_2 of both and compare them.
+
+
+#######
+# first, assign pairs of replicates (with re's?)
+# sth like:
+
+
+def get_hmp_replica_ratio(repl_1, repl_2):
+    true = 0
+    false = 0
+    replica_ratio = []
+    compare_repl = repl_1 == repl_2
+    for equal in compare_repl:
+	if equal:
+	    true += 1
+	else:
+	    false += 1
+    replica_ratio.append([true/len(repl_1), false/len(repl_1)])
+    return replica_ratio
     
-a = hmc_trim.FLFL08 == hmc_trim.FLFL08replicate
-b = hmc_trim.FLFL08 != hmc_trim.FLFL08replicate
+###
+replica_list = []
+for pop in hmp_trimmed.columns:
+    if re.findall('([A-Z]+[0-9]+[a-z]+[0-9]+)', pop):
+	continue
+    elif re.findall('([A-Z]+[0-9]+[a-z]+)', pop):
+	replica_1 = re.findall('([A-Z]+[0-9]+[a-z]+)', pop)[0]
+	replica_2 = re.findall('([A-Z]+[0-9]+)', pop)[0]
+	replica_list.append(get_hmp_replica_ratio(hmp_trimmed[replica_1], hmp_trimmed[replica_2]))
 
-np.sum(a) # gives the number of True's
+# now, I omitted the 'replicate2' ...
 
-np.sum(b)/len(hmp_trim)
-# now that's for the counts, but we're also interested in the alleles!
+col_names_replica = []
+for pop in hmp_trimmed.columns:
+    if re.findall('([A-Z]+[0-9]+[a-z]+[0-9]+)', pop):
+	continue
+    elif re.findall('([A-Z]+[0-9]+[a-z]+)', pop):
+	col_names_replica.append(re.findall('([A-Z]+[0-9]+)', pop)[0])
+
+
+# 
+for val in replica_list:
+    a.append(val[0])
+
+
+repl_ratio = pd.DataFrame(zip(*a), index= ['true', 'false'], columns= col_names_replica)
+
+
+repl_ratio.columns = pd.Index(col_names_replica) # unhashable type: list (why's that?)
+
+
+
+####################################
+
+
+hmp = pd.read_table('HapMap.hmp.txt', index_col = 0, header = 0)
+hmc = pd.read_table('HapMap.hmc.txt', index_col = 0, header = 0)
+
+# only use the actual samples
+hmp_trimmed = hmp.ix[:, 'FLFL04':'WWA30']
+
+
+alleles_MAF_results = []
+for i, col in enumerate(hmp_trimmed.columns):
+    base_list = hmp_trimmed[col]
+    count_list = hmc[hmc.columns[i]]
+    alleles_MAF_results.append(get_alleles_MAF(base_list, count_list, hmp.alleles))
+
+data_MAF = pd.DataFrame(zip(*alleles_MAF_results), index = hmp_trimmed.index, columns=hmp_trimmed.columns, dtype = np.str)
+
+
+all_pooled_values = []
+for column in data_MAF.columns:
+    for value in data_MAF.ix[:, column]:
+        all_pooled_values.append(value)
+
+zyg_types = pd.Series(get_zygosity_types(all_pooled_values))
+
+zyg_types.to_csv("allele_types2_MAF.csv")
+###########################################
+
+# adv filter + MAF
+
+adv_fil_results = []
+for i, col in enumerate(hmp_trimmed.columns):
+    base_list = hmp_trimmed[col]
+    count_list = hmc[hmc.columns[i]]
+    adv_fil_results.append(get_alleles_adv(base_list, count_list, hmp.alleles))
+
+data_adv = pd.DataFrame(zip(*adv_fil_results), index = hmp_trimmed.index, columns=hmp_trimmed.columns, dtype = np.str)
+#
+alleles_adv_MAF_results = []
+for i, col in enumerate(data_adv.columns):
+    base_list = data_adv[col]
+    count_list = hmc[hmc.columns[i]]
+    alleles_adv_MAF_results.append(get_alleles_MAF(base_list, count_list, hmp.alleles))
+
+data_adv_MAF = pd.DataFrame(zip(*alleles_adv_MAF_results), index = hmp_trimmed.index, columns=hmp_trimmed.columns, dtype = np.str)
+
+adv_MAF_pooled_values = []
+for column in data_adv_MAF.columns:
+    for value in data_adv_MAF.ix[:, column]:
+        adv_MAF_pooled_values.append(value)
+
+zyg_types = pd.Series(get_zygosity_types(adv_MAF_pooled_values))
+
+zyg_types.to_csv("allele_types2_simple_MAF.csv")
