b417d6de66bfb9b39bf3953ff9094ae8a1a61148
schimar <schimar@gmail.com>
2014-02-08 15:12:16 -0700

    add complete script for mismatch/read-depth analysis

--- replica.py
+++ replica.py
@@ -85,6 +85,96 @@ repl_summ_data = pd.DataFrame(zip(*proper), index= index, columns= col_names_rep
 repl_summ_data.to_csv("MAF_replicate_summary.csv")
 
 
+#################################################################
+# REPLICATE FUNC for filtered data
+#################################################################
+data_hmp = hmp_trimmed.copy()
+data_hmc = hmc[data_hmp]
+
+alleles_zero_results = []
+for i, col in enumerate(data_hmp.columns):
+    base_list = data_hmp[col]
+    count_list = data_hmc[data_hmc.columns[i]]
+    alleles_zero_results.append(get_alleles_zero(base_list, count_list, hmp.alleles)) # instead of alleles_drop, hmp.alleles to get all loci
+
+data_zero = pd.DataFrame(zip(*alleles_zero_results), index = data_hmp.index, columns=data_hmp.columns, dtype = np.str)
+####
+
+data = data_zero.copy()
+
+match_list_1_total, match_list_2_total, match_homozyg_total, mismatch_list_1_total, mismatch_list_2_total, mismatch_homozyg_total, mismatch_type_total = ([], [], [], [], [], [], [])
+for pop in data.columns:
+    if re.findall('([A-Z]+[0-9]+[a-z]+[0-9]+)', pop):
+	continue
+    elif re.findall('([A-Z]+[0-9]+[a-z]+)', pop):
+	replica_1 = re.findall('([A-Z]+[0-9]+[a-z]+)', pop)[0]
+	replica_2 = re.findall('([A-Z]+[0-9]+)', pop)[0]
+	match_list_1, match_list_2, match_homozyg, mismatch_list_1, mismatch_list_2, mismatch_homozyg, mismatch_type = get_replica_read_depth_minmax(data, data_hmc, replica_1, replica_2)
+	match_list_1_total.append(match_list_1)
+	match_list_2_total.append(match_list_2)
+	match_homozyg_total.append(match_homozyg)
+	mismatch_list_1_total.append(mismatch_list_1)
+	mismatch_list_2_total.append(mismatch_list_2)
+	mismatch_homozyg_total.append(mismatch_homozyg)
+	mismatch_type_total.append(mismatch_type)
+	
+
+# matches
+match_1 = get_pooled_list(match_list_1_total)
+match_2 = get_pooled_list(match_list_2_total)
+match_homo = get_pooled_list(match_homozyg_total)
+
+match = zip(*[match_1, match_2, match_homo])
+df_match = pd.DataFrame(match, columns = ['match_1', 'match_2', 'homozygote'])
+
+# match
+select_min_list, select_max_list = ([], [])
+for i, val_1 in enumerate(df_match.match_1): 
+    val_2 = df_match.match_2[i]
+    val_list = [val_1[0], val_1[1], val_2[0], val_2[1]]
+    if df_match.homozygote[i] == 1:
+	select_min = int(np.min(val_list)/2)
+	select_max = int(np.max(val_list)/2)
+    else:
+	select_min = int(np.min(val_list))
+	select_max = int(np.max(val_list))
+    select_min_list.append(select_min)
+    select_max_list.append(select_max)
+
+df_match['min'] = pd.Series(select_min_list)
+df_match['max'] = pd.Series(select_max_list)
+# df_match.to_csv{"match.csv")
+
+####
+# mismatches
+mismatch_1 = get_pooled_list(mismatch_list_1_total)
+mismatch_2 = get_pooled_list(mismatch_list_2_total)
+mismatch_homo = get_pooled_list(mismatch_homozyg_total)
+mismatch_type = get_pooled_list(mismatch_type_total)
+
+mismatch = zip(*[mismatch_1, mismatch_2, mismatch_homo, mismatch_type])
+df_mismatch = pd.DataFrame(mismatch, columns = ['mismatch_1', 'mismatch_2', 'homozygote', 'type'])
 
 
+select_min_list, select_max_list = ([], [])
+for i, val_1 in enumerate(df_mismatch.mismatch_1): 
+    val_2 = df_mismatch.mismatch_2[i]
+    val_list = [val_1[0], val_1[1], val_2[0], val_2[1]]
+    if df_mismatch.homozygote[i] == 1:
+	val_list[0] = int(val_list[0]/2)
+	val_list[1] = int(val_list[1]/2)
+	select_min = int(np.min(val_list)/2)
+	select_max = int(np.max(val_list)/2)
+    elif df_mismatch.homozygote[i] == 2:
+	val_list[2] = int(val_list[2]/2)
+	val_list[3] = int(val_list[3]/2)
+	select_min = int(np.min(val_list)/2)
+	select_max = int(np.max(val_list)/2)
+    else:
+	select_min = int(np.min(val_list))
+	select_max = int(np.max(val_list))
+    select_min_list.append(select_min)
+    select_max_list.append(select_max)
 
+df_mismatch['min'] = pd.Series(select_min_list)
+df_mismatch['max'] = pd.Series(select_max_list)
